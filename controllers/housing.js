ace85f2d7f615d73aac8cc0e91d02c8075730f6f
SowmyaReddy513 <112138420+SowmyaReddy513@users.noreply.github.com>
2022-11-16 18:20:15 -0600

    Added Styling

--- controllers/housing.js
+++ controllers/housing.js
@@ -97,3 +97,72 @@ ${JSON.stringify(req.body)}`)
 failed`); 
     } 
 }; 
+
+// Handle Hoousing delete on DELETE. 
+exports.housing_delete = async function(req, res) { 
+    console.log("delete "  + req.params.id) 
+    try { 
+        result = await Housing.findByIdAndDelete( req.params.id) 
+        console.log("Removed " + result) 
+        res.send(result) 
+    } catch (err) { 
+        res.status(500) 
+        res.send(`{"error": Error deleting ${err}}`); 
+    } 
+}; 
+
+// Handle a show one view with id specified by query 
+exports.housing_view_one_Page = async function(req, res) { 
+    console.log("single view for id "  + req.query.id) 
+    try{ 
+        result = await Housing.findById( req.query.id) 
+        res.render('housingdetail',  
+{ title: 'Housing Detail', toShow: result }); 
+    } 
+    catch(err){ 
+        res.status(500) 
+        res.send(`{'error': '${err}'}`); 
+    } 
+}; 
+
+ // Handle building the view for creating a costume. 
+// No body, no in path parameter, no query. 
+// Does not need to be async 
+exports.housing_create_Page =  function(req, res) { 
+    console.log("create view") 
+    try{ 
+        res.render('housingcreate', { title: 'House Create'}); 
+    } 
+    catch(err){ 
+        res.status(500) 
+        res.send(`{'error': '${err}'}`); 
+    } 
+};
+
+// Handle building the view for updating a costume. 
+// query provides the id 
+exports.housing_update_Page =  async function(req, res) { 
+    console.log("update view for item "+req.query.id) 
+    try{ 
+        let result = await Housing.findById(req.query.id) 
+        res.render('housingupdate', { title: 'Housing Update', toShow: result }); 
+    } 
+    catch(err){ 
+        res.status(500) 
+        res.send(`{'error': '${err}'}`); 
+    } 
+}; 
+
+// Handle a delete one view with id from query 
+exports.housing_delete_Page = async function(req, res) { 
+    console.log("Delete view for id "  + req.query.id) 
+    try{ 
+        result = await Housing.findById(req.query.id) 
+        res.render('housingdelete', { title: 'Housing Delete', toShow: 
+result }); 
+    } 
+    catch(err){ 
+        res.status(500) 
+        res.send(`{'error': '${err}'}`); 
+    } 
+};
--- routes/housing.js
+++ routes/housing.js
@@ -4,4 +4,17 @@ var router = express.Router();
  
 /* GET costumes */ 
 router.get('/', housing_controlers.housing_view_all_Page ); 
+
+/* GET detail costume page */ 
+router.get('/detail', housing_controlers.housing_view_one_Page); 
+
+/* GET create costume page */ 
+router.get('/create', housing_controlers.housing_create_Page); 
+
+/* GET create update page */ 
+router.get('/update', housing_controlers.housing_update_Page); 
+
+/* GET delete costume page */ 
+router.get('/delete', housing_controlers.housing_delete_Page); 
+ 
 module.exports = router; 
